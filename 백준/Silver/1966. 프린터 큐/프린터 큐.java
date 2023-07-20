ac8ec056cc4fb46230264dd9821949bb12de266e
Insoo-Hwang <70841847+Insoo-Hwang@users.noreply.github.com>
2023-07-20 23:18:40 +0900

    Update 프린터 큐.java

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -14,8 +14,8 @@ public class Main {
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
 
-            Queue<Integer> queue1 = new LinkedList<>();
-            List<Integer> queue2 = new LinkedList<>();
+            Queue<Integer> queue1 = new LinkedList<>(); //중요도를 순서대로 받는 queue
+            List<Integer> queue2 = new LinkedList<>();  //중요도를 내림차순으로 정렬할 queue
             st = new StringTokenizer(br.readLine());
             for(int i = 0; i < N; i++){
                 int temp = Integer.parseInt(st.nextToken());
@@ -23,20 +23,20 @@ public class Main {
                 queue2.add(temp);
             }
             Collections.sort(queue2, Collections.reverseOrder());
-            int cnt = 0;
+            int cnt = 0; //몇번째로 인쇄되는지 count
             while (true){
-                int temp = queue1.poll();
-                if(temp == queue2.get(0)){
-                    cnt++;
-                    if(M == 0)
+                int temp = queue1.poll(); //프린터 queue 맨 앞 값을 추출
+                if(temp == queue2.get(0)){ //오름차순 queue의 맨 앞 값과 같으면 
+                    cnt++; //출력하므로 count += 1
+                    if(M == 0) //만약에 M번째 인쇄물이면 중지
                         break;
-                    M--;
+                    M--; //맨 앞 인쇄물이 없어졌으므로 M-1번째로 땡겨짐
                     queue2.remove(0);
                 }
                 else{
-                    M--;
-                    if(M < 0) M = queue1.size();
-                    queue1.add(temp);
+                    M--; //앞 인쇄물이 뒤로 빠짐
+                    if(M < 0) M = queue1.size(); //만약 뒤로 빠진 인쇄물이 M번째 인쇄물이었으면 맨 뒤순번을 받음(순번은 0부터 시작)
+                    queue1.add(temp); //맨 뒤로 빠짐
                 }
             }
             System.out.println(cnt);
