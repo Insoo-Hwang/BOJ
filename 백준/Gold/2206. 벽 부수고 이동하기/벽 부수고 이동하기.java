9ed7218018dcb68ebc6c99c95403897d7d940260
Insoo-Hwang <70841847+Insoo-Hwang@users.noreply.github.com>
2023-07-27 15:01:00 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1156 ms, Memory: 213448 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -35,27 +35,26 @@ public class Main {
     }
 
     public static void BFS(){
-        boolean [][][] visited = new boolean[2][N][M];
+        boolean [][][] visited = new boolean[2][N][M]; //벽을 부순 경우와 부수지 않은 경우로 나눔
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0, 1, 0});
+        queue.add(new int[]{0, 0, 1, 0}); //0 : n, 1 : m, 2 : 경로, 3 : 부순 벽의 수
         while(!queue.isEmpty()){
             int[] now = queue.poll();
-            if(now[0] == N-1 && now[1] == M-1) {
+            if(now[0] == N-1 && now[1] == M-1) { //도착한 경우
                 min = now[2];
                 break;
             }
 
-            int tempb = now[3];
+            int tempb = now[3]; //부순 벽의 수 백업
             for(int i = 0; i < 4; i++){
                 int x = now[0] + dx[i];
                 int y = now[1] + dy[i];
                 if(x >= 0 && y >= 0 && x < N && y < M){
-                    if(!visited[now[3]][x][y] && A[x][y] == 0){
-                        visited[now[3]][x][y] = true;
+                    if(!visited[now[3]][x][y] && A[x][y] == 0){ //벽이 없는 경우
+                        visited[now[3]][x][y] = true; //해당 모드에 맞는 방문 배열에 방문 표시
                         queue.add(new int[]{x, y, now[2]+1, now[3]});
                     }
-                    else if(!visited[now[3]][x][y] && tempb == 0){
-                        visited[0][x][y] = true;
+                    else if(!visited[now[3]][x][y] && tempb == 0){ //벽이 있는데 이미 부수지 않은 경우
                         visited[1][x][y] = true;
                         queue.add(new int[]{x, y, now[2]+1, 1});
                     }
