5db2c50170a2de0f0c487e96447172b8cbfc8bd8
Insoo-Hwang <70841847+Insoo-Hwang@users.noreply.github.com>
2023-08-03 17:28:55 +0900

    Update N－Queen.java

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -23,9 +23,9 @@ public class Main {
             cnt++;
             return;
         }
-        for(int i = 0; i < N; i++){
+        for(int i = 0; i < N; i++){ //체스판의 가로 방향으로 이동, 세로 방향은 d로 확인
             if(visited[d][i] == 0){
-                check(d, i, 1);
+                check(d, i, 1); //(d, i)에 퀸을 놓았을때 퀸에 잡히는 모든 위치를 방문 처리
                 DFS(d+1);
                 check(d, i, -1);
             }
@@ -33,15 +33,15 @@ public class Main {
     }
 
     static void check(int n, int m, int b){
-        int x = n - m;
+        int x = n - m; //좌상우하 대각선 방향 확인을 위한 변수
         for(int i = 0; i < N; i++){
-            visited[i][m] += b;
-            visited[n][i] += b;
+            visited[i][m] += b; //세로 방향 방문 처리
+            visited[n][i] += b; //가로 방향 방문 처리
             if(i-x < N && i-x >= 0)
-                visited[i][i-x] += b;
-            int y = n - i;
+                visited[i][i-x] += b; //좌상우하 대각선 방문 처리
+            int y = n - i; //좌하우상 대각선 방향 확인을 위한 변수
             if(n-y < N && n-y >= 0 && m+y < N && m+y >= 0)
-                visited[n-y][m+y] += b;
+                visited[n-y][m+y] += b; //좌하우상 대각선 방문 처리
         }
     }
 }
