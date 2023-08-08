fc995cc3bb9edcf2a8817f1bf0b66802a2091b9b
Insoo-Hwang <70841847+Insoo-Hwang@users.noreply.github.com>
2023-08-08 10:33:53 +0900

    Update 전깃줄.java

--- 백준/Gold/2565. 전깃줄/전깃줄.java
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -17,19 +17,19 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             A[a] = b;
-            start = Math.min(start, a);
-            end = Math.max(end, a);
+            start = Math.min(start, a); //전기줄 시작 부분
+            end = Math.max(end, a); //전기줄 끝 부분
         }
-
-        int [] D = new int[501];
+        //LIS로 최장 증가 부분을 찾고 그 결과를 전체 전기줄 수에 빼면 제외할 전기줄 수가 나옴
+        int [] D = new int[501]; //자신을 포함한 최장 증가 부분 수열
         int max = Integer.MIN_VALUE;
         for(int i = start; i < end+1; i++){
             D[i] = 1;
-            if(A[i] == 0) continue;
+            if(A[i] == 0) continue; //전기줄이 없으면 통과
             for(int j = start; j < i; j++){
-                if(A[j] == 0) continue;
+                if(A[j] == 0) continue; //전기줄이 없으면 통과
                 if(A[i] > A[j]){
-                    D[i] = Math.max(D[i], D[j] + 1);
+                    D[i] = Math.max(D[i], D[j] + 1); //증가하는 부분일 경우 max값 갱신
                 }
             }
             max = Math.max(max, D[i]);
