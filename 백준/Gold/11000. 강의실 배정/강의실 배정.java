abcf74a1e1257079dd42fc3f965a91053522572b
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2023-08-11 16:22:16 +0900

    Update 강의실 배정.java

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -16,16 +16,16 @@ public class Main {
             int e = Integer.parseInt(st.nextToken());
             list.add(new C(s, e));
         }
-        Collections.sort(list);
+        Collections.sort(list); //종료 시간이 늦은것 부터 같다면 시작 시간이 이른것 부터
 
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder()); //역순으로 정렬해야 시작 시간이 늦은 것부터 배치됨
         for(C c : list){
             int end = c.e;
             int start = c.s;
-            if(!queue.isEmpty() && queue.peek() >= end){
-                queue.remove();
+            if(!queue.isEmpty() && queue.peek() >= end){ //빈 강의실 중 시작 시간이 늦은 것보다 끝나는 시간이 빠르면 
+                queue.remove(); //이미 들어 있는 항목 제거
             }
-            queue.add(start);
+            queue.add(start); //새로운 강의실 or 기존 강의실의 시작 시간 갱신
         }
         System.out.println(queue.size());
     }
