141961d38887f755500eacd61181abbbb8b7d324
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2023-08-15 11:33:04 +0900

    Update 평범한 배낭.java

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -11,7 +11,7 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         Bag [] A = new Bag[N+1];
-        int [][] D = new int[N+1][K+1];
+        int [][] D = new int[N+1][K+1]; //i번째 배낭을 탐색했을때 j무게 이하에서 얻을 수 있는 최대 가치
         for(int i = 1; i < N+1; i++){
             st = new StringTokenizer(br.readLine());
             A[i] = new Bag(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
@@ -19,9 +19,9 @@ public class Main {
 
         for(int i = 1; i < N+1; i++){
             for (int j = 1; j < K+1; j++){
-                D[i][j] = D[i-1][j];
-                if(j - A[i].w >= 0){
-                    D[i][j] = Math.max(D[i][j], D[i-1][j-A[i].w] + A[i].v);
+                D[i][j] = D[i-1][j]; //이전 값 가져오기
+                if(j - A[i].w >= 0){ //i번째 가방 무게가 j무게보다 가볍거나 같을 때(가방에 들어갈 수 있을 때)
+                    D[i][j] = Math.max(D[i][j], D[i-1][j-A[i].w] + A[i].v); //최대값 업데이트
                 }
             }
         }
