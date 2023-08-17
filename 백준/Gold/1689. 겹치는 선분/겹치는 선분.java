dbccab81176b54313cae961d335220565f1c0573
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2023-08-18 06:43:41 +0900

    Update 겹치는 선분.java

--- 백준/Gold/1689. 겹치는 선분/겹치는 선분.java
+++ 백준/Gold/1689. 겹치는 선분/겹치는 선분.java
@@ -16,19 +16,19 @@ public class Main {
         }
         Arrays.sort(A);
 
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
-        queue.add(A[0].s);
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder()); //내림차순 우선순위 큐
+        queue.add(A[0].s); //가장 첫 선분의 s좌표 입력
         int max = 1;
         for(int i = 1; i < N; i++){
-            if(!queue.isEmpty() && A[i].e <= queue.peek()){
-                max = Math.max(max, queue.size());
-                while(!queue.isEmpty() && A[i].e <= queue.peek()){
+            if(!queue.isEmpty() && A[i].e <= queue.peek()){ //현재 선분의 e좌표가 queue의 값보다 작다는 것은 겹치지 않는다는 것
+                max = Math.max(max, queue.size()); //현재 queue에 있는 모든 값(이전에 그었던 선분 중 겹치는 것들)을 업데이트
+                while(!queue.isEmpty() && A[i].e <= queue.peek()){ //현재 선분과 겹치지 않는 선분들 제거(queue에 있는 값이 e좌표보다 작다는 건 겹친다는 뜻)
                     queue.remove();
                 }
             }
-            queue.add(A[i].s);
+            queue.add(A[i].s); //현재 선분의 s좌표를 queue 삽입
         }
-        max = Math.max(max, queue.size());
+        max = Math.max(max, queue.size()); //마지막으로 queue에 남아있는 값들 업데이트
         System.out.println(max);
     }
     static class Line implements Comparable<Line>{
@@ -40,7 +40,7 @@ public class Main {
         }
 
         @Override
-        public int compareTo(Line o) {
+        public int compareTo(Line o) { //e좌표 기준으로 내림차순, s좌표 기준으로 오름차준
             if(this.e == o.e){
                 return this.s - o.s;
             }
