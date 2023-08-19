b23878ed3f0e6cdd86a1a05e91f6d0f064128622
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2023-08-19 17:55:52 +0900

    Update 앱.java

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -21,16 +21,16 @@ public class Main {
             c[i] = Integer.parseInt(st.nextToken());
         }
 
-        int [] D = new int [10001];
+        int [] D = new int [10001]; //i라는 코스트를 사용했을때 메모리의 최대값
         Arrays.fill(D, -1);
         for(int i = 0; i < N; i++){
             int tempc = c[i];
             for(int j = 10000; j >= tempc; j--){
-                if(D[j-tempc] != -1){
-                    D[j] = Math.max(D[j], D[j-tempc] + m[i]);
+                if(D[j-tempc] != -1){ //이전 값을 방문한적이 있을때
+                    D[j] = Math.max(D[j], D[j-tempc] + m[i]); //메모리의 최대값 업데이트
                 }
             }
-            D[tempc] = Math.max(D[tempc], m[i]);
+            D[tempc] = Math.max(D[tempc], m[i]); //i번째 앱을 종료했을때의 메모리값 업데이트
         }
         for(int i = 0; i < 10001; i++){
             if(D[i] >= M){
