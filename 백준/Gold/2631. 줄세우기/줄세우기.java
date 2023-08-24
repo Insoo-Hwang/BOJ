88f82f7984c0592fbddc0e9369ad64ccb0218148
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2023-08-24 14:24:24 +0900

    Update 줄세우기.java

--- 백준/Gold/2631. 줄세우기/줄세우기.java
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -10,10 +10,11 @@ public class Main {
 
         int N = Integer.parseInt(st.nextToken());
         int [] A = new int[N];
-        int [] D = new int[N];
+        int [] D = new int[N]; //LIS활용
         for(int i = 0; i < N; i++){
             A[i] = Integer.parseInt(br.readLine());
         }
+        //LIS알고리즘을 활용하여 최장 연속 배열을 찾고 전체 아이들 수에서 빼면 이동해야하는 아이의 수가 나옴(바르게 서있는 아이들을 제외한 나머지 아이들의 위치를 옮겨주면 됨)
 
         D[0] = 1;
         int max = 0;
