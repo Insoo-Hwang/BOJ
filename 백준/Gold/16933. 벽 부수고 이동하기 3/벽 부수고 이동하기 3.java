10cc155ad1242ffa923d99b710714db18c8d0161
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2023-08-27 16:14:38 +0900

    Update 벽 부수고 이동하기 3.java

--- 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
@@ -34,27 +34,27 @@ public class Main {
         Queue<NM> queue = new LinkedList<>();
         queue.add(new NM(0, 0, 0, 1, 0));
         boolean [][][][] visited = new boolean[N][M][K+1][2];
-        visited[0][0][0][0] = true;
+        visited[0][0][0][0] = true; //(N, M)칸에 K개의 벽을 부순 상태로 이동했을 때 시간(낮/밤)
         while(!queue.isEmpty()){
             NM now = queue.poll();
-            if(now.n == N-1 && now.m == M-1){
-                min = now.c;
+            if(now.n == N-1 && now.m == M-1){ //(N, M)에 도착하면
+                min = now.c; //값 업데이트
                 return;
             }
             for(int i = 0; i < 4; i++){
                 int tempn = now.n + dy[i];
                 int tempm = now.m + dx[i];
                 if(tempn < 0 || tempn > N-1 || tempm < 0 || tempm > M-1) continue;
-                if(A[tempn][tempm] == 1 && now.k < K && !visited[tempn][tempm][now.k+1][1] && now.t == 0){
-                    queue.add(new NM(tempn, tempm, now.k+1, now.c+1, 1));
-                    visited[tempn][tempm][now.k+1][1] = true;
+                if(A[tempn][tempm] == 1 && now.k < K && !visited[tempn][tempm][now.k+1][1] && now.t == 0){ //이동했는데 벽이 있는 경우 + 벽을 부술 수 있는 상황
+                    queue.add(new NM(tempn, tempm, now.k+1, now.c+1, 1)); //벽을 부수고 이동하면 밤으로 바뀜
+                    visited[tempn][tempm][now.k+1][1] = true; //이동한 곳에서는 밤
                 }
-                else if(A[tempn][tempm] == 1 && now.k < K && !visited[now.n][now.m][now.k][0] && now.t == 1){
-                    queue.add(new NM(now.n, now.m, now.k, now.c+1, 0));
+                else if(A[tempn][tempm] == 1 && now.k < K && !visited[now.n][now.m][now.k][0] && now.t == 1){ //이동했는데 벽이 있는 경우 + 벽을 부술 수 없는 상황
+                    queue.add(new NM(now.n, now.m, now.k, now.c+1, 0)); //현재 위치에서 낮으로 바뀌면 이동(현재 위치에서 밤)
                     visited[now.n][now.m][now.k][0] = true;
                 }
-                else if(A[tempn][tempm] == 0 && !visited[tempn][tempm][now.k][(now.t+1)%2]) {
-                    queue.add(new NM(tempn, tempm, now.k, now.c+1, (now.t+1)%2));
+                else if(A[tempn][tempm] == 0 && !visited[tempn][tempm][now.k][(now.t+1)%2]) { //벽이 없는 경우
+                    queue.add(new NM(tempn, tempm, now.k, now.c+1, (now.t+1)%2)); //이동 후 낮과 밤만 바뀜
                     visited[tempn][tempm][now.k][(now.t+1)%2] = true;
                 }
             }
@@ -62,11 +62,11 @@ public class Main {
     }
 
     static class NM{
-        int n;
-        int m;
-        int k;
-        int c;
-        int t;
+        int n; //N좌표
+        int m; //M좌표
+        int k; //부순 벽의 수
+        int c; //이동한 칸의 수
+        int t; //낮과 밤 여부(낮 : 0, 밤 : 1)
         public NM(int n, int m, int k, int c, int t){
             this.n = n;
             this.m = m;
