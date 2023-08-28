a5b81ffc013b7cee3d0c511ff11f8270deb2d69d
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2023-08-28 23:55:28 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 280 ms, Memory: 17236 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -7,7 +7,11 @@ public class Main {
     static int N;
     static int M;
     static char [][] A;
-    static int min = Integer.MAX_VALUE;
+    static int [] blue = new int[2];
+    static int [] red = new int[2];
+    static int [] dy = {-1, 1, 0, 0};
+    static int [] dx = {0, 0, -1, 1};
+    static int cnt = 11;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,158 +24,59 @@ public class Main {
             for(int j = 0; j < M; j++){
                 char temp = s.charAt(j);
                 A[i][j] = temp;
+                if(temp == 'B'){
+                    A[i][j] = '.';
+                    blue[0] = i;
+                    blue[1] = j;
+                }
+                if(temp == 'R'){
+                    A[i][j] = '.';
+                    red[0] = i;
+                    red[1] = j;
+                }
             }
         }
-        char [][] B = new char[N][M];
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                B[i][j] = A[i][j];
-            }
+
+        for(int i = 0; i < 4; i++){
+            DFS(blue[0], blue[1], red[0], red[1], 1, i);
         }
-        move(1, 1); //위쪽으로 이동
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                A[i][j] = B[i][j];
-            }
-        }
-        move(1, 2); //아래쪽으로 이동
-        for(int i = 0; i < N; i++){
-         향
-        if(d == 11){
+        if(cnt == 11) System.out.println(-1);
+        else System.out.println(cnt);
+    }
+
+    static void DFS(int nB, int mB, int nR, int mR, int c, int a){
+        if(c > 10){
             return;
         }
-        int tempRed = -1;
-        if(a == 1) { //위쪽으로 이동
-            for (int i = 1; i < N-1; i++) {
-                for (int j = 1; j < M-1; j++) {
-                    int n = i;
-                    int m = j;
-                    if (A[i][j] == '#' || A[i][j] == 'O' || A[i][j] == '.') continue; //공만 움직일 수 있도록
-                    while (true) {
-                        if(n < 1 || n > N-2 || m < 1 || m > M-2) break; //게임기의 범위를 넘어가면 break
-                        if (A[n-1][m] == '.') { //빈공간이 있으면 그 쪽으로 이동
-                            A[n-1][m] = A[n][m];
-                            A[n][m] = '.';
-                            n--;
-                        } else if (A[n-1][m] == '#' || A[n-1][m] == 'B' || A[n-1][m] == 'R') { //가로막히면 이동 중지
-                            break;
-                        } else if (A[n-1][m] == 'O' && A[n][m] == 'B') { //구멍에 파란 공이 들어가면 게임 종료
-                            return;
-                        } else if (A[n-1][m] == 'O' && A[n][m] == 'R') { //구멍에 빨간 공이 들어가면 d(횟수) 업데이트, 파란 공이 추가로 들어가면 return될 수 있도록 min에는 업데이트 x
-                            tempRed = d;
-                            A[n][m] = '.';
-                            break;
-                        }
-                    }
-                }
+        int blue = 0;
+        int red = 0;
+        while(A[nB+dy[a]][mB+dx[a]] != '#'){
+            nB+=dy[a];
+            mB+=dx[a];
+            blue++;
+            if(A[nB][mB] == 'O') return;
+        }
+        while(A[nR+dy[a]][mR+dx[a]] != '#'){
+            nR+=dy[a];
+            mR+=dx[a];
+            red++;
+            if(A[nR][mR] == 'O'){
+                cnt = Math.min(cnt, c);
             }
         }
-        else if(a == 2) { //아래쪽으로 이동
-            for (int i = N-2; i > 0; i--) {
-                for (int j = 1; j < M-1; j++) {
-                    int n = i;
-                    int m = j;
-                    if (A[i][j] == '#' || A[i][j] == 'O' || A[i][j] == '.') continue;
-                    while (true) {
-                        if(n < 1 || n > N-2 || m < 1 || m > M-2) break;
-                        if (A[n+1][m] == '.') {
-                            A[n+1][m] = A[n][m];
-                            A[n][m] = '.';
-                            n++;
-                        } else if (A[n+1][m] == '#' || A[n+1][m] == 'B' || A[n+1][m] == 'R') {
-                            break;
-                        } else if (A[n+1][m] == 'O' && A[n][m] == 'B') {
-                            return;
-                        } else if (A[n+1][m] == 'O' && A[n][m] == 'R') {
-                            tempRed = d;
-                            A[n][m] = '.';
-                            break;
-                        }
-                    }
-                }
+        if(nB == nR && mB == mR){
+            if(blue > red){
+                nB-=dy[a];
+                mB-=dx[a];
             }
-        }
-        else if(a == 3) { //왼쪽으로 이동
-            for (int i = 1; i < N-1; i++) {
-                for (int j = 1; j < M-1; j++) {
-                    int n = i;
-                    int m = j;
-                    if (A[i][j] == '#' || A[i][j] == 'O' || A[i][j] == '.') continue;
-                    while (true) {
-                        if(n < 1 || n > N-2 || m < 1 || m > M-2) break;
-                        if (A[n][m-1] == '.') {
-                            A[n][m-1] = A[n][m];
-                            A[n][m] = '.';
-                            m--;
-                        } else if (A[n][m-1] == '#' || A[n][m-1] == 'B' || A[n][m-1] == 'R') {
-                            break;
-                        } else if (A[n][m-1] == 'O' && A[n][m] == 'B') {
-                            return;
-                        } else if (A[n][m-1] == 'O' && A[n][m] == 'R') {
-                            tempRed = d;
-                            A[n][m] = '.';
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        else if(a == 4) { //오른쪽으로 이동
-            for (int i = 1; i < N-1; i++) {
-                for (int j = M-2; j > 0; j--) {
-                    int n = i;
-                    int m = j;
-                    if (A[i][j] == '#' || A[i][j] == 'O' || A[i][j] == '.') continue;
-                    while (true) {
-                        if(n < 1 || n > N-2 || m < 1 || m > M-2) break;
-                        if (A[n][m+1] == '.') {
-                            A[n][m+1] = A[n][m];
-                            A[n][m] = '.';
-                            m++;
-                        } else if (A[n][m+1] == '#' || A[n][m+1] == 'B' || A[n][m+1] == 'R') {
-                            break;
-                        } else if (A[n][m+1] == 'O' && A[n][m] == 'B') {
-                            return;
-                        } else if (A[n][m+1] == 'O' && A[n][m] == 'R') {
-                            tempRed = d;
-                            A[n][m] = '.';
-                            break;
-                        }
-                    }
-                }
+            else{
+                nR-=dy[a];
+                mR-=dx[a];
             }
         }
 
-
-        if(tempRed != -1){ //파란 공은 들어가지 않고 빨간 공만 들어가면
-            min = Math.min(min, tempRed); //min 업데이트
-        }
-        else{
-            char [][] B = new char[N][M];
-            for(int i = 0; i < N; i++){
-                for(int j = 0; j < M; j++){
-                    B[i][j] = A[i][j];
-                }
-            }
-            move(d+1, 1); //위쪽으로 이동
-            for(int i = 0; i < N; i++){
-                for(int j = 0; j < M; j++){
-                    A[i][j] = B[i][j];
-                }
-            }
-            move(d+1, 2); //아래쪽으로 이동
-            for(int i = 0; i < N; i++){
-                for(int j = 0; j < M; j++){
-                    A[i][j] = B[i][j];
-                }
-            }
-            move(d+1, 3); //왼쪽으로 이동
-            for(int i = 0; i < N; i++){
-                for(int j = 0; j < M; j++){
-                    A[i][j] = B[i][j];
-                }
-            }
-            move(d+1, 4); //오른쪽으로 이동
+        for(int i = 0; i < 4; i++){
+            DFS(nB, mB, nR, mR, c+1, i);
         }
     }
 }
