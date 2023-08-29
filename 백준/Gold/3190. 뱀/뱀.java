0f07de9a6ce561056b3660687a461cd02fa6f69b
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2023-08-29 16:19:58 +0900

    Update 뱀.java

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -32,22 +32,22 @@ public class Main {
         int [] head = {1, 1};
         while(true){
             if(!list.isEmpty()) {
-                limit = list.get(0);
+                limit = list.get(0); //방향 바꿀 시간 및 방향 확인
             }
-            if(limit.x == time){
+            if(limit.x == time){ //방향을 바꿀 시간이 되면
                 list.remove(0);
                 time++;
-                if(limit.c.equals("L")){
-                    if(now == 'R'){
-                        if(head[0] == 1 || snake[head[0]-1][head[1]]){
+                if(limit.c.equals("L")){ //왼쪽으로 방향 바꾸기
+                    if(now == 'R'){ //현재 방향이 오른쪽일 때
+                        if(head[0] == 1 || snake[head[0]-1][head[1]]){ //더 이상 이동할 수 없는 경우(벽에 부딪히거나 자신의 몸에 닿았을 때)
                             break;
                         }
-                        snake[head[0]-1][head[1]] = true;
-                        now = 'U';
+                        snake[head[0]-1][head[1]] = true; //위쪽 방향으로 이동
+                        now = 'U'; //현재 방향 업데이트
                         head[0]-=1;
-                        snakelist.add(new int[] {head[0], head[1]});
+                        snakelist.add(new int[] {head[0], head[1]}); //꼬리 체크를 위해 이동한 칸 업데이트
                     }
-                    else if(now == 'U'){
+                    else if(now == 'U'){ //현재 방향이 위쪽일 때
                         if(head[1] == 1 || snake[head[0]][head[1]-1]){
                             break;
                         }
@@ -56,7 +56,7 @@ public class Main {
                         head[1]-=1;
                         snakelist.add(new int[] {head[0], head[1]});
                     }
-                    else if(now == 'L'){
+                    else if(now == 'L'){ //현재 방향이 왼쪽일 때
                         if(head[0] == N || snake[head[0]+1][head[1]]){
                             break;
                         }
@@ -65,7 +65,7 @@ public class Main {
                         head[0]+=1;
                         snakelist.add(new int[] {head[0], head[1]});
                     }
-                    else{
+                    else{ //현재 방향이 아래쪽일 때
                         if(head[1] == N || snake[head[0]][head[1]+1]){
                             break;
                         }
@@ -75,7 +75,7 @@ public class Main {
                         snakelist.add(new int[] {head[0], head[1]});
                     }
                 }
-                else{
+                else{ //오른른쪽으로 방향 바꾸기
                     if(now == 'R'){
                         if(head[0] == N || snake[head[0]+1][head[1]]){
                             break;
@@ -103,58 +103,7 @@ public class Main {
                         head[0]-=1;
                         snakelist.add(new int[] {head[0], head[1]});
                     }
-                    else{
-                        if(head[1] == N || snake[head[0]][head[1]+1]){
-                            break;
-                        }
-                        snake[head[0]][head[1]+1] = true;
-                        now = 'R';
-                        head[1]+=1;
-                        snakelist.add(new int[] {head[0], head[1]});
-                    }
-                }
-            }
-            else{
-                time++;
-                if(now == 'R'){
-                    if(head[1] == N || snake[head[0]][head[1]+1]){
-                        break;
-                    }
-                    snake[head[0]][head[1]+1] = true;
-                    head[1]+=1;
-                    snakelist.add(new int[] {head[0], head[1]});
-                }
-                else if(now == 'L'){
-                    if(head[1] == 1 || snake[head[0]][head[1]-1]){
-                        break;
-                    }
-                    snake[head[0]][head[1]-1] = true;
-                    head[1]-=1;
-                    snakelist.add(new int[] {head[0], head[1]});
-                }
-                else if(now == 'U'){
-                    if(head[0] == 1 || snake[head[0]-1][head[1]]){
-                        break;
-                    }
-                    snake[head[0]-1][head[1]] = true;
-                    head[0]-=1;
-                    snakelist.add(new int[] {head[0], head[1]});
-                }
-                else{
-                    if(head[0] == N || snake[head[0]+1][head[1]]){
-                        break;
-                    }
-                    snake[head[0]+1][head[1]] = true;
-                    head[0]+=1;
-                    snakelist.add(new int[] {head[0], head[1]});
-                }
-            }
-            if(apple[head[0]][head[1]]){
-                apple[head[0]][head[1]] = false;
-            }
-            else{
-                int [] temp = snakelist.remove(0);
-                snake[temp[0]][temp[1]] = false;
+                    else로
             }
         }
         System.out.println(time);
