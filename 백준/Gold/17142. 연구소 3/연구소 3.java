ece171b42afbf449f05307d004a9c54471a53c42
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2023-08-30 20:49:57 +0900

    Update 연구소 3.java

--- 백준/Gold/17142. 연구소 3/연구소 3.java
+++ 백준/Gold/17142. 연구소 3/연구소 3.java
@@ -19,8 +19,8 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         A = new int[N][N];
-        virus = new int[N*N][2];
-        tempV = 0;
+        virus = new int[N*N][2]; //전체 바이러스의 위치를 저장하는 배열
+        tempV = 0; //전체 바이러스의 개수
         for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < N; j++){
@@ -38,41 +38,41 @@ public class Main {
         if(min == Integer.MAX_VALUE) System.out.println(-1);
         else System.out.println(min);
     }
-    static void DFS(int d, int s){
-        if(d == M){
-            BFS();
+    static void DFS(int d, int s){ //d는 선택된 바이러스의 수, s는 바이러스 배열에서 탐색 중인 위치
+        if(d == M){ //활성 바이러스를 M개 선택하면
+            BFS(); //바이러스 퍼뜨리기 시작
             return;
         }
-        if(s == tempV) return;
+        if(s == tempV) return; //탐색 범위를 넘어가면 종료
 
-        A[virus[s][0]][virus[s][1]] = 3;
+        A[virus[s][0]][virus[s][1]] = 3; //현재 순서의 바이러스를 선택하는 케이스
         DFS(d+1, s+1);
-        A[virus[s][0]][virus[s][1]] = 2;
+        A[virus[s][0]][virus[s][1]] = 2; //현재 순서의 바이러스를 선택하지 않는 케이스
         DFS(d, s+1);
     }
 
     static void BFS(){
         boolean [][] visited = new boolean[N][N];
         Queue<Virus> queue = new LinkedList<>();
-        int check = N*N;
+        int check = N*N; //전체 칸을 전염시켰는지 확인하기 위한 변수
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
-                if(A[i][j] == 3){
+                if(A[i][j] == 3){ //활성 바이러스가 있는 경우
                     visited[i][j] = true;
                     queue.add(new Virus(i, j, 0));
                     check--;
                 }
-                else if(A[i][j] == 2){
-                    check--;
+                else if(A[i][j] == 2){ //비활성 바이러스가 있는 경우
+                    check--; //이미 전염되어 있기 때문에
                 }
-                else if(A[i][j] == 1){
-                    visited[i][j] = true;
-                    check--;
+                else if(A[i][j] == 1){ //벽이 있는 경우
+                    visited[i][j] = true; //벽에는 접근이 불가능
+                    check--; //벽은 전염 불가
                 }
             }
         }
 
-        int max = 0;
+        int max = 0; //시간의 최대값(전부 전염시키는데에 걸리는 시간)
         while(!queue.isEmpty()){
             Virus now = queue.poll();
             for(int i = 0; i < 4; i++){
@@ -81,21 +81,21 @@ public class Main {
                 if(y < 0 || y > N-1 || x < 0 || x > N-1 || visited[y][x]) continue;
                 visited[y][x] = true;
                 queue.add(new Virus(y, x, now.t+1));
-                if(A[y][x] != 2){
+                if(A[y][x] != 2){ //비활성 바이러스는 전염시간이 0초이기 때문에 시간 업데이트 x
                     max = Math.max(max, now.t+1);
                     check--;
                 }
             }
         }
-        if(check == 0){
-            min = Math.min(max, min);
+        if(check == 0){ //모든 칸이 전염되면
+            min = Math.min(max, min); //최소 시간 업데이트
         }
     }
 
     static class Virus{
-        int n;
-        int m;
-        int t;
+        int n; //n좌표
+        int m; //m좌표
+        int t; //걸린 시간
         public Virus(int n, int m, int t){
             this.n = n;
             this.m = m;
