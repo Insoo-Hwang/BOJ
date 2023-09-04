f4fcd434b11142779151b15f06051c9a07ba48e0
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2023-09-04 16:14:26 +0900

    Update 색종이 붙이기.java

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class Main {
     static int [][] A = new int[10][10];
-    static int [] paper = new int[6];
+    static int [] paper = new int[6]; //사이즈 별로 남은 색종이 수 체크
     static int min = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,25 +25,25 @@ public class Main {
         else System.out.println(min);
     }
 
-    static void DFS(int n, int m, int c){
+    static void DFS(int n, int m, int c){ //(n, m)부터 붙일 수 있는 색종이 수 탐색 c는 이미 사용된 색종이 수
         if(check(n, m, 10, 1)){
             min = Math.min(min, c);
             return;
         }
 
-        for(int i = 1; i < 6; i++){
+        for(int i = 1; i < 6; i++){ //1~5 사이즈의 색종이를 붙일 수 있는지 확인
             if(check(n, m, i, 0) && paper[i] > 0){
-                paper[i]--;
-                change(n, m, i, 0);
+                paper[i]--; //해당 사이즈 색종이 수 감소
+                change(n, m, i, 0); //색종이를 붙임
                 int [] temp = next();
-                DFS(temp[0], temp[1], c+1);
-                paper[i]++;
+                DFS(temp[0], temp[1], c+1); //다음 공간 탐색
+                paper[i]++; //백트래킹
                 change(n, m, i, 1);
             }
         }
     }
 
-    static boolean check(int n, int m, int s, int v){
+    static boolean check(int n, int m, int s, int v){ //(n, m)부터 s크기의 만큼 v가 있는지 확인
         if(n+s > 10 || m+s > 10) return false;
         for(int i = n; i < n+s; i++){
             for(int j = m; j < m+s; j++){
@@ -53,7 +53,7 @@ public class Main {
         return true;
     }
 
-    static void change(int n, int m, int s, int v){
+    static void change(int n, int m, int s, int v){ (n, m)부터 s크기의 만큼 v로 교체
         for(int i = n; i < n+s; i++){
             for(int j = m; j < m+s; j++){
                 A[i][j] = v;
@@ -61,7 +61,7 @@ public class Main {
         }
     }
 
-    static int[] next(){
+    static int[] next(){ //다음에 색종이를 붙일 수 있는 공간 탐색
         int [] temp = new int[2];
         for(int i = 0; i < 10; i++){
             for(int j = 0; j < 10; j++){
