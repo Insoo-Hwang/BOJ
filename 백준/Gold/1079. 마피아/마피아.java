3f661b44778e7862a951e47ee1952bd68e600d37
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2023-09-04 18:48:34 +0900

    Update 마피아.java

--- 백준/Gold/1079. 마피아/마피아.java
+++ 백준/Gold/1079. 마피아/마피아.java
@@ -5,11 +5,11 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static int [] A;
-    static boolean [] alive;
-    static int [][] R;
-    static int E;
-    static int max = 0;
+    static int [] A; //유죄지수
+    static boolean [] alive; //살아있는지 여부
+    static int [][] R; //반응
+    static int E; //마피아
+    static int max = 0; //버틸 수 있는 밤의 수의 최대값
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -35,30 +35,30 @@ public class Main {
         System.out.println(max);
     }
 
-    static void DFS(int c, int p){
-        if(!alive[E] || p == 1){
+    static void DFS(int c, int p){ //c는 보낸 밤의 수, p는 남은 사람의 수
+        if(!alive[E] || p == 1){ //마피아가 죽거나 남은 사람이 1명이면 종료
             max = Math.max(max, c);
             return;
         }
 
-        if(p%2 == 0){
+        if(p%2 == 0){ //밤
             for(int i = 0; i < N; i++){
-                if(!alive[i] || i == E) continue;
-                change(i, 1);
-                alive[i] = false;
+                if(!alive[i] || i == E) continue; //죽어있거나 마피아가 죽는 경우는 패스
+                change(i, 1); //유죄지수 변경
+                alive[i] = false; //선택된 시민 사망
                 DFS(c+1, p-1);
                 alive[i] = true;
                 change(i, -1);
             }
         }
-        else{
-            int max = 0;
-            int idx = N-1;
+        else{ //낮
+            int max = 0; //유죄지수의 최대값
+            int idx = N-1; //인덱스값
             for(int i = 0; i < N; i++) {
-                if(alive[i] && max < A[i]) {
-                    max = A[i];
-                    idx = i;
-                }else if(alive[i] && max == A[i]) {
+                if(alive[i] && max < A[i]) { //살아있으면서 유죄지수가 높으면
+                    max = A[i]; //유죄지수 업데이트
+                    idx = i; //인덱스 업데이트
+                }else if(alive[i] && max == A[i]) { //유죄지수가 같으면 인덱스가 작은 사람 사망
                     max = A[i];
                     idx = Math.min(i, idx);
                 }
