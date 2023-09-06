c896e3c7f2f5f88101e903d807b6ec963d01551b
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2023-09-06 16:45:40 +0900

    Update 주사위 윷놀이.java

--- 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.java
+++ 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.java
@@ -35,41 +35,41 @@ public class Main {
         yoots[32] = new Yoot(0, -1, 0);
         yoots[5].jump = 21;
         yoots[10].jump = 25;
-        yoots[15].jump = 27;
+        yoots[15].jump = 27; //맵
 
         DFS(0, 0);
         System.out.println(max);
     }
 
-    static void DFS(int c, int s){
+    static void DFS(int c, int s){ //c는 주사위를 굴릴 횟수, s는 얻을 수 있는 최대 점수
         if(c == 10){
             max = Math.max(max, s);
             return;
         }
 
-        int dice = A[c];
-        for(int i = 0; i < 4; i++){
-            int prev = mal[i];
-            if(prev == 32) continue;
-            int now = move(prev, dice);
-            if(check(now)){
+        int dice = A[c]; //주사위를 굴렸을 때 나올 숫자
+        for(int i = 0; i < 4; i++){ //말 4개를 움직일 수 있음
+            int prev = mal[i]; //원래 말의 위치
+            if(prev == 32) continue; //말이 결승점에 도달하면 움직이지 않음
+            int now = move(prev, dice); //말이 주사위 숫자만큼 움직일시의 위치
+            if(check(now)){ //현재 위치에 말을 놓을 수 있으면
                 mal[i] = now;
-                DFS(c+1, s+yoots[now].score);
-                mal[i] = prev;
+                DFS(c+1, s+yoots[now].score); //계속 진행
+                mal[i] = prev; //백트래킹
             }
         }
     }
 
-    static int move(int o, int c){
+    static int move(int o, int c){ //o는 현재 위치, c는 주사위 숫자
         int temp = o;
-        if(yoots[o].jump != 0){
+        if(yoots[o].jump != 0){ //파란 화살표로 이동 가능하면 이동
             temp = yoots[o].jump;
             for(int i = 1; i < c; i++){
                 temp = yoots[temp].next;
-                if(temp == 32) break;
+                if(temp == 32) break; //결승점 도달시 더 이상 이동 x
             }
         }
-        else{
+        else{ //빨간 화살표로 이동
             for(int i = 0; i < c; i++){
                 temp = yoots[temp].next;
                 if(temp == 32) break;
@@ -86,9 +86,9 @@ public class Main {
     }
 
     static class Yoot{
-        int score;
-        int next;
-        int jump;
+        int score; //점수
+        int next; //다음 위치
+        int jump; //이 자리에 위치한 경우 이동할 수 있는 다음 위치(파란 화살표)
         public Yoot(int score, int next, int jump){
             this.score = score;
             this.next = next;
