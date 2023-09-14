5293aee24ac2bc5b33c8f25f2371c2c239f221bb
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2023-09-14 15:39:10 +0900

    Update 컨베이어 벨트 위의 로봇.java

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -17,27 +17,27 @@ public class Main {
         int cnt = 1;
         while(true){
             Belt tb = list.remove(list.size()-1);
-            list.add(0, tb);
-            list.get(N-1).b = false;
-            for(int i = N-2; i >= 0; i--){
+            list.add(0, tb); //컨베이어 벨트를 오른쪽으로 이동
+            list.get(N-1).b = false; //내리는 곳에서 박스를 내림
+            for(int i = N-2; i >= 0; i--){ //박스가 이동 가능하면 한칸 이동
                 if(list.get(i).b && !list.get(i+1).b && list.get(i+1).n > 0){
                     list.get(i).b = false;
                     list.get(i+1).b = true;
                     list.get(i+1).n--;
-                    if(list.get(i+1).n == 0){
+                    if(list.get(i+1).n == 0){ //내구성이 0이 되면 K감소
                         K--;
                     }
                 }
             }
-            list.get(N-1).b = false;
-            if(list.get(0).n > 0){
+            list.get(N-1).b = false; //내리는 곳에서 박스를 내림
+            if(list.get(0).n > 0){ //올리는 곳에서 박스를 올림
                 list.get(0).b = true;
                 list.get(0).n--;
-                if(list.get(0).n == 0){
+                if(list.get(0).n == 0){ //내구성이 0이 되면 K감소
                     K--;
                 }
             }
-            if(K <= 0){
+            if(K <= 0){ //내구성이 0인 벨트가 K개 이상이면 중지
                 break;
             }
             cnt++;
@@ -46,8 +46,8 @@ public class Main {
     }
 
     static class Belt{
-        int n;
-        boolean b;
+        int n; //벨트의 내구성
+        boolean b; //박스 유무
         public Belt(int n, boolean b){
             this.n = n;
             this.b = b;
