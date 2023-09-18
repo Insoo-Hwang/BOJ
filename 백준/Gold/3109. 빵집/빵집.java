05033c8e0afdd10a06da7a0d27742af46b220eb3
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2023-09-18 15:42:37 +0900

    Update 빵집.java

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -6,7 +6,7 @@ public class Main {
     static int C;
     static int [][] A;
     static boolean [][] visited;
-    static int [] dy = {-1, 0, 1};
+    static int [] dy = {-1, 0, 1}; //오른쪽 위, 오른쪽, 오른쪽 아래로만 이동 가능
     static int [] dx = {1, 1, 1};
     static int cnt = 0;
     static boolean check;
@@ -24,12 +24,12 @@ public class Main {
                     A[i][j] = 0;
                 }
                 else{
-                    A[i][j] = 1;
+                    A[i][j] = 1; //건물이 있는 곳
                 }
             }
         }
         visited = new boolean[R][C];
-        for(int i = 0; i < R; i++){
+        for(int i = 0; i < R; i++){ //1열의 모든 행에서 출발
             check = false;
             DFS(i, 0);
         }
@@ -39,7 +39,7 @@ public class Main {
     static void DFS(int n, int m){
         if(m == C-1){
             cnt++;
-            check = true;
+            check = true; //배관 건설에 성공시
             return;
         }
 
@@ -47,9 +47,9 @@ public class Main {
             int y = n + dy[i];
             int x = m + dx[i];
             if(y < 0 || y > R-1 || x < 0 || x > C-1 || visited[y][x] || A[y][x] == 1) continue;
-            visited[y][x] = true;
+            visited[y][x] = true; //배관 건설에 실패하더라도 한 번 방문한 곳은 다시 방문해도 건설 불가능
             DFS(y, x);
-            if(check){
+            if(check){ //건설 성공시 더 이상 시도 x
                 break;
             }
         }
