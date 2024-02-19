d1b85a5398d034a4b6eceec30be48e8d481a3d18
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2024-02-19 13:50:11 +0900

    [Gold I] Title: 외판원 순회, Time: 316 ms, Memory: 63144 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -17,8 +17,8 @@ public class Main {
         int [][] D = new int[N][1<<N];
         for(int i = 0; i < N; i++)  Arrays.fill(D[i], Integer.MAX_VALUE);
         D[0][1] = 0;
-        PriorityQueue<SalesMan> queue = new PriorityQueue<>();
-        queue.add(new SalesMan(0, 0, 1));
+        Queue<SalesMan> queue = new LinkedList<>();
+        queue.add(new SalesMan(0, 1));
         while(!queue.isEmpty()){
             SalesMan now = queue.poll();
             if(now.v == (1<<N)-1) {
@@ -31,27 +31,20 @@ public class Main {
                 if((now.v & (1<<i)) > 0 || W[now.l][i] == 0) continue;
                 if(D[i][now.v | (1<<i)] > D[now.l][now.v]+W[now.l][i]){
                     D[i][now.v | (1<<i)] = D[now.l][now.v]+W[now.l][i];
-                    queue.add(new SalesMan(i, D[i][now.v | (1<<i)], now.v | (1<<i)));
+                    queue.add(new SalesMan(i, now.v | (1<<i)));
                 }
             }
         }
         System.out.println(D[0][(1<<N)-1]);
     }
 
-    static class SalesMan implements Comparable<SalesMan>{
+    static class SalesMan{
         int l;
-        int c;
         int v;
 
-        public SalesMan(int l, int c, int v){
+        public SalesMan(int l, int v){
             this.l = l;
-            this.c = c;
             this.v = v;
-        }
-
-        @Override
-        public int compareTo(SalesMan o){
-            return this.c-o.c;
         }
     }
 }
