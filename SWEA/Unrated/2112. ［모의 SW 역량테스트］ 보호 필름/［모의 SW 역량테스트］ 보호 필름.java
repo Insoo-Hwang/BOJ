9fabccc5b9bc5ceb4cc7d815d1f1f229905d9f4b
HWANG INSOO <70841847+Insoo-Hwang@users.noreply.github.com>
2024-10-12 13:34:40 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 591 ms, Memory: 97,680 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -1,71 +1,75 @@
 import java.util.*;
-import java.io.*;
 
 public class Solution {
-	static int D;
-	static int W;
-	static int K;
-	static int [][] A;
-	static int min;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			D = sc.nextInt();
-			W = sc.nextInt();
-			K = sc.nextInt();
-			A = new int [D][W];
-			for(int i = 0; i < D; i++) {
-				for(int j = 0; j < W; j++) {
-					A[i][j] = sc.nextInt();
-				}
-			}
-			min = Integer.MAX_VALUE;
-			DFS(0, 0);
-			System.out.println("#" + test_case + " " + min);
-		}
-	}
-	
-	static void DFS(int cnt, int n) {
-		if(cnt >= min) return;
-		if(n == D) {
-			if(TEST()) {
-				min = Math.min(min, cnt);
-			}
-			return;
-		}
-		int [] B = new int[W];
-		for(int j = 0; j < W; j++) {
-			B[j] = A[n][j];
-		}
-		for(int j = 0; j < W; j++) {
-			A[n][j] = 0;
-		}
-		DFS(cnt+1, n+1);
-		for(int j = 0; j < W; j++) {
-			A[n][j] = 1;
-		}
-		DFS(cnt+1, n+1);
-		for(int j = 0; j < W; j++) {
-			A[n][j] = B[j];
-		}
-		DFS(cnt, n+1);
-	}
-	
-	static boolean TEST() {
-		for(int i = 0; i < W; i++) {
-			int max = 0;
-			int cnt = 1;
-			for(int j = 1; j < D; j++) {
-				if(A[j][i] == A[j-1][i]) cnt++;
-				else cnt = 1;
-				max = Math.max(cnt, max);
-				if(max >= K) break;
-			}
-			if(max < K) return false;
-		}
-		return true;
-	}
+    static int D;
+    static int W;
+    static int K;
+    static int min;
+    static int [][] A;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int T = sc.nextInt();
+        for(int t = 1; t < T+1; t++){
+            D = sc.nextInt();
+            W = sc.nextInt();
+            K = sc.nextInt();
+            A = new int[D][W];
+            for(int i = 0; i < D; i++){
+                for(int j = 0; j < W; j++){
+                    A[i][j] = sc.nextInt();
+                }
+            }
+            min = Integer.MAX_VALUE;
+            DFS(0, 0);
+            sb.append("#").append(t).append(" ").append(min).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void DFS(int d, int m){
+        if(d == D){
+            if(test()) min = Math.min(min, m);
+            return;
+        }
+
+        DFS(d+1, m);
+
+        if(min > m) {
+            int[] temp = new int[W];
+            for (int i = 0; i < W; i++) {
+                temp[i] = A[d][i];
+                A[d][i] = 0;
+            }
+            DFS(d + 1, m + 1);
+            for (int i = 0; i < W; i++) A[d][i] = temp[i];
+
+            for (int i = 0; i < W; i++) {
+                temp[i] = A[d][i];
+                A[d][i] = 1;
+            }
+            DFS(d + 1, m + 1);
+            for (int i = 0; i < W; i++) A[d][i] = temp[i];
+        }
+    }
+
+    private static boolean test() {
+        for(int i = 0; i < W; i++){
+            int cnt = 0;
+            int prev = -1;
+            int max = 0;
+            for(int j = 0; j < D; j++){
+                if(A[j][i] == prev) cnt++;
+                else{
+                    prev = A[j][i];
+                    cnt = 1;
+                }
+                max = Math.max(max, cnt);
+                if(max >= K) break;
+            }
+            if(max < K) return false;
+        }
+        return true;
+    }
 }
